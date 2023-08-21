845bbbc4cb8dab53eb02ab8948b81d3c1995d2ea
Cedric Ruault <cedric.ruault215@gmail.com>
2023-08-21 12:55:48 +0200

    old files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create(name: 'Reload', address: '11 rue ober', phone_number: '0122345', category: 'chinese')
+Restaurant.create(name: 'Circus', address: '11 rue reuilly', phone_number: '552763', category: 'italian')
+Restaurant.create(name: 'Apple', address: '254 rue plume', phone_number: '704375', category: 'japanese')
+Restaurant.create(name: 'Matcha', address: '17 rue ambroise', phone_number: '734682', category: 'french')
+Restaurant.create(name: 'QG', address: '102 rue saint-maur', phone_number: '0187653', category: 'belgian')
+
+puts 'Finished!'
